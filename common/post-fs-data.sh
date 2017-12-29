3c7ae82c49a8a8eb12362756bc26ed2901371eaf
Zackptg5 <zackptg5@comcast.net>
2017-12-29 13:33:06 -0500

    bring up to date

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -1,10 +1,4 @@
-#!/system/bin/sh
-# Please don't hardcode /magisk/modname/... ; instead, please use $MODDIR/...
-# This will make your scripts compatible even if Magisk change its mount point in the future
-MODDIR=${0%/*}
-
-# This script will be executed in post-fs-data mode
-# More info in the main Magisk thread
-
-# Enable SELinux Permissive mode at boot
-setenforce 0
+set_sepolicy $SOURCE init unix_stream_socket connectto
+set_sepolicy $SOURCE property_socket sock_file getattr,open,read,write,execute
+set_sepolicy $SOURCE
+set_sepolicy audio_prop
--- version.sh
+++
@@ -1,7 +0,0 @@
-revision() {
-revision="3"
-}
-
-version() {
-version="2.2"
-}
